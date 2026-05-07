3bece96f535145838661f438cc38c7f230f6ec98
valentinaAguirrehu <valentinaagui2006@gmail.com>
2026-05-07 13:24:39 -0500

    correo envios

--- mi_examen_fianl/settings.py
+++ mi_examen_fianl/settings.py
@@ -51,13 +51,15 @@ INSTALLED_APPS = [
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-# 📧 EMAIL CONFIG (Console for testing)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'angiebenavides1809@gmail.com'
-# EMAIL_HOST_PASSWORD = 'gfhv ucwq ojoz ueqp'
+# 📧 EMAIL CONFIG
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'angiebenavides1809@gmail.com'
+EMAIL_HOST_PASSWORD = 'gfhv ucwq ojoz ueqp'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
